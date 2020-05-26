d090e6820d906034389486a97bd9a7c01c2084b1
totiospfc <65943927+totiospfc@users.noreply.github.com>
2020-05-26 04:41:39 -0300

    Add files via upload

---
+++ pesquisas_1.js
@@ -0,0 +1,11 @@
+var json_pesquisas_1 = {
+"type": "FeatureCollection",
+"name": "pesquisas_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "autor": "Luiz Costa", "povo": "Kanamari", "lattes": "xxxx" }, "geometry": { "type": "Point", "coordinates": [ -71.065281616408328, -6.023535880273394 ] } },
+{ "type": "Feature", "properties": { "id": null, "autor": "Carlos Fausto", "povo": "Parakana", "lattes": "xxxx" }, "geometry": { "type": "Point", "coordinates": [ -52.030291956813301, -5.750858342449996 ] } },
+{ "type": "Feature", "properties": { "id": null, "autor": "Thiago Oliveira", "povo": "Baniwa", "lattes": "xxxx" }, "geometry": { "type": "Point", "coordinates": [ -68.752766343713731, 0.174634306404652 ] } },
+{ "type": "Feature", "properties": { "id": null, "autor": "Victor Gil", "povo": "Tyonwük-dyapa", "lattes": "xxxx" }, "geometry": { "type": "Point", "coordinates": [ -69.659943536856971, -5.771833537667185 ] } }
+]
+}
